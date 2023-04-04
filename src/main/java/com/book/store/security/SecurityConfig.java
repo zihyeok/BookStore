f84f22417fd0be5616ec509f9ab4fbd7e9ba306d
nojimi656 <qkrqkrgur@naver.com>
2023-04-04 17:44:05 +0900

    BookController test1 by park

--- src/main/java/com/book/store/security/SecurityConfig.java
+++ src/main/java/com/book/store/security/SecurityConfig.java
@@ -1,26 +1,26 @@
-package com.book.store.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-	
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		
-		
-		http
-		.authorizeHttpRequests().antMatchers("/**").permitAll();
-		
-		
-		
-		return http.build();
-		
-		
-		
-	}
-
-}
+//package com.book.store.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//	
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//		
+//		
+//		http
+//		.authorizeHttpRequests().antMatchers("/**").permitAll();
+//		
+//		
+//		
+//		return http.build();
+//		
+//		
+//		
+//	}
+//
+//}
