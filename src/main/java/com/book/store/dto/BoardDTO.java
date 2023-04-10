771dd032cf428bcfdafe9a15b91c4b5a08d062d3
zihyeok <zihyeok@naver.com>
2023-04-10 18:26:21 +0900

    수정만하면 된다

--- src/main/java/com/book/store/dto/BoardDTO.java
+++ src/main/java/com/book/store/dto/BoardDTO.java
@@ -7,10 +7,11 @@ public class BoardDTO {
 	
 	private int num;
 	private String name;
+	
 	private String email;
 	private String subject;
 	private String content;
-	private String ipAddr;
+
 	private String created;
 	private int hitCount;
 }
