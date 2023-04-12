694838cc7563ded0aafb7bb58f45601a36d57325
nojimi656 <qkrqkrgur@naver.com>
2023-04-12 12:05:12 +0900

    ajax like

--- src/main/java/com/book/store/controller/BagController.java
+++ src/main/java/com/book/store/controller/BagController.java
@@ -1,15 +1,17 @@
 package com.book.store.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.book.store.dto.BagDTO;
+import com.book.store.dto.BookDTO;
 import com.book.store.service.BagService;
 import com.book.store.service.BagServiceImpl;
 import com.book.store.user.UserData;
@@ -21,49 +23,95 @@ import lombok.RequiredArgsConstructor;
 public class BagController {
 
 	private final HttpSession httpSession;
-	
+
 	@Resource
 	BagService bagService = new BagServiceImpl();
-	
-	@GetMapping("/selectItem.action")
+
+	@RequestMapping("/selectItem.action")
 	public ModelAndView selectItem(HttpServletRequest request,BagDTO dto) throws Exception{
+				//여기에 request 말고도 매개변수로 bagdto dto를 받게되면 ajax로 넘어올때 bagdto정보도 같이 넘어옴
 
 		UserData user = null;
-		
-		String searchKey = request.getParameter("searchKey");
-		String searchValue = request.getParameter("searchValue");
-		String pageNum = request.getParameter("pageNum");
+
 		int seq_No = Integer.parseInt(request.getParameter("seq_No"));
-		
+
 		if(httpSession.getAttribute("user")!="") {
-			
+
 			user = (UserData) httpSession.getAttribute("user");
-			
+
+
 		}else if(httpSession.getAttribute("OauthUser")!="") {
-			
+
 			user = (UserData) httpSession.getAttribute("OauthUser");
-			
-		}else {
-			
-			ModelAndView mav = new ModelAndView();
-			
-			mav.setViewName("redirect:/user/loginpage");
-			
-			return mav;
-			
+
 		}
+		
+		if(user==null) {
+		//else if는 안되고 if는 됨
+			ModelAndView mav = new ModelAndView();
+
+			mav.setViewName("redirect:/user/login");
+
+			return mav;
+
+		}
+		
+		String userId = user.getUserId();
 		
 		dto.setBagId(bagService.maxNum()+1);
 		dto.setSeq_No(seq_No);
+		dto.setUserId(userId);
+
+		bagService.insertData(dto);
 		
-		System.out.println(user.getUserId());
-		dto.setUserId(user.getUserId());
-		
+		List<BookDTO> lists = bagService.getLists(userId);
+
 		ModelAndView mav = new ModelAndView("jsonView");
 		//ajax로 가져와서 userid의 seq_no를 조회후 해당 seq_no가 있으면 찜 버튼에 불이 들어와있게 설정해야됨
-		
-		return mav;
-		
+		//getLists 처음에는 하위쿼리로 실행하려했으나 하위쿼리로 가져오는 값이 2개이상이 되어서 에러가남
+		//그래서 inner join사용함
+		mav.addObject("lists", lists);
+		//ajax로 booklist.html로 lists 쏠거임
+		return mav;		
+
 	}
 	
+	@RequestMapping("/allItem.action")
+	public ModelAndView allItem(HttpServletRequest request) throws Exception{
+		
+		UserData user = null;
+
+		if(httpSession.getAttribute("user")!="") {
+
+			user = (UserData) httpSession.getAttribute("user");
+
+
+		}else if(httpSession.getAttribute("OauthUser")!="") {
+
+			user = (UserData) httpSession.getAttribute("OauthUser");
+
+		}
+		
+		if(user==null) {
+			
+			ModelAndView mav = new ModelAndView();
+
+			mav.setViewName("redirect:/user/login");
+
+			return mav;
+
+		}
+		
+		String userId = user.getUserId();
+
+		List<BookDTO> lists = bagService.getLists(userId);
+
+		ModelAndView mav = new ModelAndView("jsonView");
+		
+		mav.addObject("lists", lists);
+		
+		return mav;		
+		
+	}
+
 }
