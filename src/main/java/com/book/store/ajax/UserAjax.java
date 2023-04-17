b3881ba69427258e1c3eed9213d7490149c739b7
minsung <hjms777@naver.com>
2023-04-17 20:33:37 +0900

    minsung

--- src/main/java/com/book/store/ajax/UserAjax.java
+++ src/main/java/com/book/store/ajax/UserAjax.java
@@ -167,6 +167,9 @@ public class UserAjax {
 			
 			//결제완료 페이지에서 보여질 구매한 List데이터 추가
 			lists.add(bookitemservice.getReadData(Integer.parseInt(title_No)));
+			
+			//결제완료후 VIP점수 누적
+			userService.updateVip(user.getUserId());
 		}
 		
 		ModelAndView mav = new ModelAndView();
